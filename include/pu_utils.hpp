9136d111c1531e9f648651e1343d1c3acef1e0b7
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-02-21 12:53:06 +0100

    [ INCLUDE ] FlowPU macro utilities

---
+++ include/pu_utils.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+// ======== registers ======== 
+
+#define AX 0
+#define BX 1
+#define CX 2
+#define DX 3
+#define EX 4
+#define FX 5
+#define GX 6
+#define HX 7
+
+#define LHA 8
+#define LHB 9
+
+#define ERL 10
+
+#define SP 11
+#define IP 12
+
+#define TMPA 13
+#define TMPB 14
+#define TMPC 15
+
+#define H 1
+#define L 0
