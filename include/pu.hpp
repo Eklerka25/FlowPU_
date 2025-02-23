156c0f18e2336c3070f114e41e16f09bd62c69f4
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-02-24 00:08:57 +0100

    [ INCLUDE ] main header update

--- include/pu.hpp
+++ include/pu.hpp
@@ -1,10 +1,23 @@
 #pragma once
 
-#include <cstdint>
+#include <pu_utils.hpp>
 
 class Fsis {
     public:
-        uint8_t reg_write(int index, int half, int8_t value);
-        uint8_t reg_read(int index, int half);
-        uint8_t reg_free(int index, int half);
+        Byte reg_write(int index, int half, Byte value);
+        Byte reg_read(int index, int half);
+        Byte reg_free(int index, int half);
+
+        void Reset();
+        void FlagSet(Byte flag);
+        u32 FlagGet();
+};
+
+class Memory {
+    public:
+        void alloc(int index, Byte value);
+        Byte read(int index);
+        Byte free(int index);
+        
+        u32 getsize();
 };
