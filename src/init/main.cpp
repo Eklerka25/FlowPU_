bbf46f4ef2adc19fb52c1d09850379484100699a
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-02-24 00:06:25 +0100

    [ INIT ] Memory showcase (main.cpp)

--- src/init/main.cpp
+++ src/init/main.cpp
@@ -7,8 +7,15 @@ using std::cout;
 
 int main() {
     Fsis cpu;
+    Memory memory;
+
+    cpu.Reset();
     
     cpu.reg_write(AX, H, 0xFF);
-    cout << static_cast<int>(cpu.reg_read(FLAG, H)) << nl;
-    cout << static_cast<int>(cpu.FlagGet()) << nl;
+    cout << "CPU            AX : " << static_cast<int>(cpu.reg_read(AX, H)) << nl;
+    cout << "CPU          FLAG : " << static_cast<int>(cpu.FlagGet()) << nl;
+
+    memory.alloc(10, 0xff);
+    cout << "MEMORY 0x0000000A : " << static_cast<int>(memory.read(10)) << nl;
+    cout << "CPU          FLAG : " << static_cast<int>(cpu.FlagGet()) << nl;
 }
