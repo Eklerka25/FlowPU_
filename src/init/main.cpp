9533d0a008e2edebccb44142ec476ab7cfe9ded9
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-02-23 23:44:46 +0100

    [ INIT ] New main.cpp

--- src/init/main.cpp
+++ src/init/main.cpp
@@ -3,13 +3,12 @@
 
 #include <iostream>
 
-int main() {
-    Fsis fsis;
+using std::cout;
 
-    fsis.reg_write(AX, H, 0xFA);
-    std::cout << static_cast<int>(fsis.reg_read(AX, H)) << "\n";
-    fsis.reg_write(AX, H, 0xAA);
-    std::cout << static_cast<int>(fsis.reg_read(AX, H)) << "\n";
-    fsis.reg_free(AX, H);
-    std::cout << static_cast<int>(fsis.reg_read(AX, H)) << "\n";
+int main() {
+    Fsis cpu;
+    
+    cpu.reg_write(AX, H, 0xFF);
+    cout << static_cast<int>(cpu.reg_read(FLAG, H)) << nl;
+    cout << static_cast<int>(cpu.FlagGet()) << nl;
 }
