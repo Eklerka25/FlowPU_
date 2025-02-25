a2d10ac6d348e72bae3703a3b5042620e7b43ece
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-02-25 01:40:19 +0100

    [ CPU ] 16 bit -> 8 bit

--- src/cpu/reg.cpp
+++ src/cpu/reg.cpp
@@ -3,31 +3,28 @@
 #include <pu_utils.hpp>
 #include <pu.hpp>
 
-Byte reg[16][2];
+Byte reg[16];
 
-Byte Fsis::reg_write(int index, int half, Byte value) {
-    Fsis cpu;
+Byte Fsis::reg_write(int index, Byte value) {
     if (index < 16 && index >= 0) {
-        reg[index][half] = value;
+        reg[index] = value;
     } else {
         this->FlagSet(FLAG_OUTOFRANGE);
     }
 }
 
-Byte Fsis::reg_read(int index, int half) {
-    Fsis cpu;
+Byte Fsis::reg_read(int index) {
     if (index >= 0 && index < 16) {
-        return reg[index][half];
+        return reg[index];
     } else {
         this->FlagSet(FLAG_OUTOFRANGE);
         return 0;
     } 
 }
 
-Byte Fsis::reg_free(int index, int half) {
-    Fsis cpu;
+Byte Fsis::reg_free(int index) {
     if (index < 16 && index >= 0) {
-        reg[index][half] = 0x0;
+        reg[index] = 0x0;
         this->FlagSet(FLAG_SUCCES);
     } else {
         this->FlagSet(FLAG_OUTOFRANGE);
