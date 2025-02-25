d97628670d0b4e71723e230b4b1c28f902da3653
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-02-25 11:40:34 +0100

    [ INIT ] update

--- src/init/main.cpp
+++ src/init/main.cpp
@@ -7,15 +7,16 @@ using std::cout;
 
 int main() {
     Fsis cpu;
-    Memory memory;
+    Ram ram;
+    Rom rom;
 
     cpu.Reset();
-    
-    cpu.reg_write(AX, H, 0xFF);
-    cout << "CPU            AX : " << static_cast<int>(cpu.reg_read(AX, H)) << nl;
-    cout << "CPU          FLAG : " << static_cast<int>(cpu.FlagGet()) << nl;
 
-    memory.alloc(10, 0xff);
-    cout << "MEMORY 0x0000000A : " << static_cast<int>(memory.read(10)) << nl;
-    cout << "CPU          FLAG : " << static_cast<int>(cpu.FlagGet()) << nl;
+    cpu.reg_write(AX, 0b10110011);
+    cpu.reg_write(BX, 0b01111111);
+    cout << "CPU                [92mAX[0m : " << static_cast<int>(cpu.reg_read(AX)) << nl;
+    cout << "CPU                [92mBX[0m : " << static_cast<int>(cpu.reg_read(BX)) << nl;
+    cpu.math_add(AX, BX, CX);
+    cout << "CPU                [92mCX[0m : " << static_cast<int>(cpu.reg_read(CX)) << nl;
+    cout << "CPU              [92mFLAG[0m : " << static_cast<int>(cpu.FlagGet()) << nl;
 }
